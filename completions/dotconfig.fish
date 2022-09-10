2f450c8ca23bd98a7464a5c89c9773dcccabdb4c
Masato Bito <m@btmst.jp>
2022-09-10 18:42:36 +0900

    fix completion

--- completions/dotconfig.fish
+++ completions/dotconfig.fish
@@ -1,6 +1,7 @@
-complete -xc dotconfig -n __fish_use_subcommand -a clone -d "Clone dotconfig config repository"
-complete -xc dotconfig -n __fish_use_subcommand -a help -d "Show usage help"
-complete -xc dotconfig -n __fish_use_subcommand -a init -d "Initialize all packages"
-complete -xc dotconfig -n __fish_use_subcommand -a load -d "Load script(config.fish for default) of all modules"
-complete -xc dotconfig -n __fish_use_subcommand -a set_path -d "Set path variable"
-complete -xc dotconfig -n __fish_use_subcommand -a setup -d "Clone, Initialize, Load"
+complete -c dotconfig --no-files
+complete -c dotconfig -n __fish_use_subcommand -a clone    -d "Clone dotconfig config repository"
+complete -c dotconfig -n __fish_use_subcommand -a help     -d "Show usage help"
+complete -c dotconfig -n __fish_use_subcommand -a init     -d "Initialize all packages"
+complete -c dotconfig -n __fish_use_subcommand -a load     -d "Load script(config.fish for default) of all modules"
+complete -c dotconfig -n __fish_use_subcommand -a set_path -d "Set path variable"
+complete -c dotconfig -n __fish_use_subcommand -a setup    -d "Clone, Initialize, Load"
